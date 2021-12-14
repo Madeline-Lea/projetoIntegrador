33338eaf0d3669136f30a64c1279108084c89422
Vanessa Vitoria <75499157+Vanessarocha10@users.noreply.github.com>
2021-12-14 16:32:31 -0300

    Add files via upload
    Criação da rota de erros

--- routes/erroRouter.js
+++ routes/erroRouter.js
@@ -1,4 +1,14 @@
 const express = require("express");
 const erroRouter = express.Router();
 
+erroRouter.get("/", (req,res)=>{
+res.render("../views/errors/401")
+res.render("../views/errors/403")
+res.render("../views/errors/404")
+})
+
+erroRouter.post("/",(req,res)=>{
+
+})
+
 module.exports = erroRouter
