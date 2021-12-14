0f2a2a7d6cf0b0497a0d5dc65ab0f8f51b5f4f85
Vanessa Vitoria <75499157+Vanessarocha10@users.noreply.github.com>
2021-12-14 16:38:21 -0300

    Criação da rota de linguagens

--- routes/linguagensRouter.js
+++ routes/linguagensRouter.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const linguagensRouter = express.Router()
 
+linguagensRouter.get("/",(req,res)=>{
+res.render("../views/feedTopicoseLinguagens/linguagens/perfil");
+})
+
 
 
 
